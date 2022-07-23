dc18d74a93e523b51f3c4b1158509dd7913d5214
Camilla Hossein <108483716+camillahossein@users.noreply.github.com>
2022-07-23 15:09:47 -0300

    Add files via upload

---
+++ variacoes-ifelse2.js
@@ -0,0 +1,18 @@
+/* 
+  Considerando que temos um número aleatório de 1 a 100, quero que
+  você crie uma verificação onde verifique se esse número é 42.
+  Caso seja 42, imprima "É 42!"
+  Caso não seja 42, imprima "Não é 42"
+*/
+
+// Código base para utilizar:
+// Crie sua solução abaixo:
+
+let numero = 66;
+if (numero === 42) {
+    console.log("É 42!");
+} else {
+    console.log("Não é 42");
+}
+
+//esse nao rodou, nao entendi porque //
