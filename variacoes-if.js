94d05d9d2a329f06a02613db69b18a8eb46492c7
Camilla Hossein <108483716+camillahossein@users.noreply.github.com>
2022-07-23 14:35:47 -0300

    Add files via upload
    feito primeiro exercicio

---
+++ variacoes-if.js
@@ -0,0 +1,25 @@
+
+/* 
+  Supondo que temos um número entre 0 a 10, quero que crie três verificações:
+  - Onde caso ele seja maior que 2, imprima no console "é maior que 2".
+  - Caso seja maior que 5, imprima no console "é maior que 5".
+  - E caso seja maior que 8, imprima no console "é maior que 8".
+  Detalhe: Você deve poder imprimir mais de um resultado.
+  Ex: Caso seja 6, ele deve imprimir "é maior que 2" e "é maior que 5"
+*/
+
+// Código base para utilizar:
+// const number = Math.floor(Math.random() * 10)
+
+// Crie sua solução abaixo:
+
+let number = 6;
+    if (number >2) {
+    console.log("é maior que 2");
+}
+    if (number >5) {
+        console.log("é maior que 5");
+}
+    if (number >8) {
+    console.log("[e maior que 8");
+}
